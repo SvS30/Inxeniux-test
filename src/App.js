534a99fe42fe1f5215214de2bd318aeb8636c1ff
salimv <salimvzqz@gmail.com>
2023-01-18 16:39:17 -0600

    User index in Datatable init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import { MDBDataTable } from 'mdbreact';
+import React from 'react';
 
 function App() {
+
+  const [users, setUsers] = React.useState();
+
+  const fetchApiBackend = async () => {
+    const response = await fetch('http://localhost:80/api/users')
+    const responseJson = await response.json()
+    if (responseJson) setUsers(responseJson.users)
+  }
+
+  React.useEffect(() => {
+    fetchApiBackend();
+  }, []);
+
+  const data = {
+    columns: [
+      {
+        label: 'Name',
+        field: 'name',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Age',
+        field: 'age',
+        sort: 'asc',
+        width: 270
+      },
+      {
+        label: 'Phone',
+        field: 'phone',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'Country',
+        field: 'country',
+        sort: 'asc',
+        width: 100
+      }
+    ],
+    rows: users
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MDBDataTable
+      striped
+      bordered
+      hover
+      data={data}
+    />
   );
 }
 
